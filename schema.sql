9bd10868b100332a8e811fc7a5a0b22e39a6e0a5
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-07-11 21:15:47 +0300

    Add primary key and add new column species

--- schema.sql
+++ schema.sql
@@ -1,10 +1,19 @@
 /* Database schema to keep the structure of entire database. */
 
+-- recreate the table with adding primary keys
 CREATE TABLE animals (
-  id INT,
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(250),
   date_of_birth DATE,
   escape_attempts INT,
   neutered BOOL,
   weight_kg DECIMAL
 );
+
+PRIMARY KEY (id)
+
+
+
+--add species
+ALTER TABLE animals ADD COLUMN species VARCHAR;
+
