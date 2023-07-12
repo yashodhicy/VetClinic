fdc09a1506fbb87cbad237e5153caa3cf69ae1c1
yashodhi Chathurangi <yashodhichathurangi94@gmail.com>
2023-07-12 20:23:58 +0300

    add owners and species tables, add foreign keys

--- schema.sql
+++ schema.sql
@@ -17,3 +17,32 @@ PRIMARY KEY (id)
 --add species
 ALTER TABLE animals ADD COLUMN species VARCHAR;
 
+--create owners table
+CREATE TABLE owners (
+  id SERIAL PRIMARY KEY,
+  full_name VARCHAR(255),
+  age INT
+);
+
+ -- create species table
+CREATE TABLE species (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+-- update animals table
+ALTER TABLE animals DROP COLUMN species;
+
+
+ALTER TABLE animals ADD species_id INT,
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+
+ALTER TABLE animals ADD owner_id INT,
+ADD CONSTRAINT fk_owners
+FOREIGN KEY (owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
